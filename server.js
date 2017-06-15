4045e59a16d591cf7ca9335c724c14f11c985ac9
Kevin Madill <kmadill@gmail.com>
2017-06-15 15:23:27 +0200

    minor cleanup

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+// Use multer({ dest: './uploads/' }) if you wish to actually save files (not saving here, so argument left blank)
 app.post('/', multer({ }).single('upload'), function (req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({size: req.file.size}));
